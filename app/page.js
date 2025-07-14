87a88d33450cb01df4cf47921f15678bea0a7014
Anishkachawla <davanishka10i10@gmail.com>
2025-07-15 02:12:04 +0530

    Navbar added

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       I am main
     </main>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
+import logoimg from '../assets/logo.png'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='bg-black border border-b-2 border-violet-800 text-white rounded-full mx-25 my-5 p-4 flex justify-between'>
+        <Image src={logoimg} alt='logo' height={2} width={100}></Image>
+        <ul className='flex gap-4 font-bold text-lg p-3'>
+            <Link href="/"><li>Home</li></Link>
+            <Link href="/"><li>About</li></Link>
+            <Link href="/"><li>Contact Us</li></Link>
+        </ul>
+    </nav>
   )
 }
 
