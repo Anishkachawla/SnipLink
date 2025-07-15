08708704c93e0ae3e5cf1712330aea6d9c6bde3b
Anishkachawla <davanishka10i10@gmail.com>
2025-07-16 00:38:10 +0530

    Main page added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
 import Image from "next/image";
+import vectorimg from '../assets/vector.jpg'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      I am main
+      <section className="flex justify-center items-center px-25 py-10">
+        <div className="w-[450] justify-end mr-8">
+          <Image src={vectorimg} alt="img" height={500} width={1000} className="rounded-4xl"></Image>
+        </div>
+        <div className="text-white items-end">
+          <p className="text-3xl font-bold">Instantly Shorten and Share Your Links!</p>
+          <p className="text-lg">Make your long URLs short and sweet. Our free URL shortener helps you create concise links for easy sharing.</p>
+          <div className="items-center justify-center my-2"><Link href="/generate"><button className='rounded-4xl bg-violet-800 font-bold px-10 py-3 cursor-pointer'>Try Now</button></Link></div>
+        </div>
+      </section>
     </main>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -6,12 +6,15 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav className='bg-black border border-b-2 border-violet-800 text-white rounded-full mx-25 my-5 p-4 flex justify-between'>
-        <Image src={logoimg} alt='logo' height={2} width={100}></Image>
-        <ul className='flex gap-4 font-bold text-lg p-3'>
+        <Image src={logoimg} alt='logo' className='brightness-175' height={2} width={100}></Image>
+        <div className='flex gap-4'>
+          <ul className='flex gap-4 font-bold text-lg p-3'>
             <Link href="/"><li>Home</li></Link>
-            <Link href="/"><li>About</li></Link>
-            <Link href="/"><li>Contact Us</li></Link>
+            <Link href="/about"><li>About</li></Link>
+            <Link href="/contact"><li>Contact Us</li></Link>
         </ul>
+        <Link href="/generate"><button className='rounded-4xl bg-violet-800 font-bold px-4 py-3 cursor-pointer'>Try Now</button></Link>
+        </div>
     </nav>
   )
 }
